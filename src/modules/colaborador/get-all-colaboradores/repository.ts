2bada6dcec013eca80aa34152f5337c5769e63e1
GabrielQuintiliano <greisquintiliano@gmail.com>
2024-02-14 16:23:13 -0300

    Refactor getAll method in AllColaboradoresRepository

--- src/modules/colaborador/get-all-colaboradores/repository.ts
+++ src/modules/colaborador/get-all-colaboradores/repository.ts
@@ -7,11 +7,17 @@ export class AllColaboradoresRepository implements AllColaboradoresInterface {
     async getAll(): Promise<unknown> {
         try {
             const knex = this.knexAdapter.getKnexInstance();
-            const getAll = await knex('db_fast.colaboradores as c')
-            .join('db_fast.atas as a', 'c.Id', '=', 'a.ColaboradorId')
-            .join('db_fast.workshops as w', 'a.WorkshopId', '=', 'w.Id')
-            .select('c.Nome as Colaborador', 'w.Nome as Workshop', 'w.DataRealizacao', 'w.Descricao')
-            .orderBy('c.Nome');
+            const getAll = await knex('db_fast.colaborador as C')
+            .leftJoin('db_fast.ata_colaborador as AC', 'C.Id', 'AC.ColaboradorId')
+            .leftJoin('db_fast.ata as A', 'AC.AtaId', 'A.Id')
+            .leftJoin('db_fast.workshop as W', 'A.WorkshopId', 'W.Id')
+            .select([
+                'C.Nome as ColaboradorNome',
+                'W.Nome as WorkshopNome',
+                'W.DataRealizacao as DataRealizacaoWorkshop',
+                'W.Descricao as DescricaoWorkshop'
+            ])
+            .orderBy('C.Nome');
 
             return getAll
         } catch (error) {
