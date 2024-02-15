175438365f43952557dddb90b6912698ed705eb3
GabrielQuintiliano <greisquintiliano@gmail.com>
2024-02-15 15:09:55 -0300

    Refactor getAtaByDate and getAtaByWorkshop repositories

--- src/modules/ata/getAtaByDate/repository.ts
+++ src/modules/ata/getAtaByDate/repository.ts
@@ -8,19 +8,37 @@ export class AtaByDateRepository implements IAtaByDateInterface {
     async getAtaByDate(props: AtaByDateModel): Promise<any> {
         try {
             const knex = this.knexAdapter.getKnexInstance();
-            const getAta = knex('db_fast.workshop as W')
-            .join('db_fast.ata as A', 'W.Id', 'A.WorkshopId')
-            .join('db_fast.ata_Colaborador as AC', 'A.Id', 'AC.AtaId')
-            .join('db_fast.colaborador as C', 'AC.ColaboradorId', 'C.Id')
-            .select([
-                'W.Nome as WorkshopNome',
-                'W.DataRealizacao as DataRealizacaoWorkshop',
-                'W.Descricao as DescricaoWorkshop',
-                'C.Nome as ColaboradorNome'
-            ])
-            .where('W.DataRealizacao', props.data);
 
-            return getAta
+            if (props.data) {
+                const getAta = knex('db_fast.workshop as W')
+                    .join('db_fast.ata as A', 'W.Id', 'A.WorkshopId')
+                    .join('db_fast.ata_Colaborador as AC', 'A.Id', 'AC.AtaId')
+                    .join('db_fast.colaborador as C', 'AC.ColaboradorId', 'C.Id')
+                    .select([
+                        'A.Id AS AtaId',
+                        'W.DataRealizacao as DataRealizacaoWorkshop',
+                        'W.Descricao as DescricaoWorkshop',
+                        'W.Nome as WorkshopNome',
+                        'C.Nome as ColaboradorNome'
+                    ])
+                    .where('W.DataRealizacao', props.data);
+
+                return getAta
+            } else {
+                const getAtaWithoutData = knex('db_fast.ata as A')
+                    .join('db_fast.workshop as W', 'A.WorkshopId', 'W.Id')
+                    .join('db_fast.ata_Colaborador as AC', 'A.Id', 'AC.AtaId')
+                    .join('db_fast.colaborador as C', 'AC.ColaboradorId', 'C.Id')
+                    .select([
+                        'A.Id AS AtaId',
+                        'W.DataRealizacao as DataRealizacaoWorkshop',
+                        'W.Descricao as DescricaoWorkshop',
+                        'W.Nome AS WorkshopNome',
+                        'C.Nome AS ColaboradorNome'
+                    ])
+
+                return getAtaWithoutData
+            }
         } catch (error) {
             console.log(error);
         }
--- src/modules/ata/getAtaByWorkshop/repository.ts
+++ src/modules/ata/getAtaByWorkshop/repository.ts
@@ -8,19 +8,40 @@ export class AtaByWorkshopRepository implements IAtaByWorkshopInterface {
     async getAtaByWorkshop(props: AtaByWorkshopModel): Promise<any> {
         try {
             const knex = this.knexAdapter.getKnexInstance();
-            const getAta = knex('db_fast.workshop as W')
-            .join('db_fast.ata as A', 'W.Id', 'A.WorkshopId')
-            .join('db_fast.ata_Colaborador as AC', 'A.Id', 'AC.AtaId')
-            .join('db_fast.colaborador as C', 'AC.ColaboradorId', 'C.Id')
-            .select([
-                'W.Nome as WorkshopNome',
-                'W.DataRealizacao as DataRealizacaoWorkshop',
-                'W.Descricao as DescricaoWorkshop',
-                'C.Nome as ColaboradorNome'
-            ])
-            .where('W.Nome', props.workshopNome);
 
-            return getAta
+            if(props.workshopNome) {
+                const getAta = knex('db_fast.workshop as W')
+                .join('db_fast.ata as A', 'W.Id', 'A.WorkshopId')
+                .join('db_fast.ata_Colaborador as AC', 'A.Id', 'AC.AtaId')
+                .join('db_fast.colaborador as C', 'AC.ColaboradorId', 'C.Id')
+                .select([
+                    'A.Id AS AtaId',
+                    'W.DataRealizacao as DataRealizacaoWorkshop',
+                    'W.Descricao as DescricaoWorkshop',
+                    'W.Nome as WorkshopNome',
+                    'C.Nome as ColaboradorNome',
+                ])
+                .where(knex.raw('lower(W.Nome)'), 'like', `%${props.workshopNome.toLowerCase()}%`);
+    
+                return getAta
+            } else {
+                const getAtaWithoutData = knex('db_fast.ata as A')
+                .join('db_fast.workshop as W', 'A.WorkshopId', 'W.Id')
+                .join('db_fast.ata_Colaborador as AC', 'A.Id', 'AC.AtaId')
+                .join('db_fast.colaborador as C', 'AC.ColaboradorId', 'C.Id')
+                .select([
+                    'A.Id AS AtaId',
+                    'W.DataRealizacao as DataRealizacaoWorkshop',
+                    'W.Descricao as DescricaoWorkshop',
+                    'W.Nome AS WorkshopNome',
+                    'C.Nome AS ColaboradorNome'
+                ])
+
+                return getAtaWithoutData
+            }
+    
+
+            
         } catch (error) {
             console.log(error);
         }
